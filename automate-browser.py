9afdba079247c9684f9a21215aa37bad37f601f5
Vinicius Braga <Vbragadev@gmail.com>
2024-10-18 19:50:17 -0300

    [ADD] the function that next page to find the energy bill

--- automate-browser.py
+++ automate-browser.py
@@ -2,7 +2,7 @@ from time import sleep
 from helium import *
 import os 
 from dotenv import load_dotenv
-
+from selenium.webdriver.common.by import By
 
 
 load_dotenv()
@@ -10,7 +10,7 @@ load_dotenv()
 login = os.getenv('login')
 password = os.getenv('password')
 
-start_firefox('https://www.cpfl.com.br/login')
+driver = start_firefox('https://www.cpfl.com.br/login')
 
 #sleep for 1.5 seconds
 sleep(1)
@@ -31,14 +31,34 @@ write(login, into='Endereço de e-mail')
 # If value is correct write password.
 write(password, into='Senha')
 
+#Signin
 click('Entrar')
 
-sleep(5)
-click('Ativa')
+
+#load the page complete.
+sleep(7)
+
+element_label_option = driver.find_elements(By.CLASS_NAME, "option")
 
 
+#click in the element 'ativa'
+for element in element_label_option:
+    element.click()
 
+#wait seconds to appear the button 'Avançar'
+sleep(2)
 
+#find element 'Avançar'
+next = driver.find_element(By.ID, "btn-buscar")
+
+#scrool until button 'Avançar'
+driver.execute_script("arguments[0].scrollIntoView(true);", next)
+
+#wait to after scroll to element.
+sleep(0.8)
+
+#click the element.
+next.click()
         
 
 
